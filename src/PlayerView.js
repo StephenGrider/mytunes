c323d78a759c3a73a93ed9df76ecbfaecd8ccd2b
Shawn Drost <sdrost@gmail.com>
2012-12-13 14:53:19 -0800

    minor fixes

--- src/PlayerView.js
+++ src/PlayerView.js
@@ -2,7 +2,7 @@ var PlayerView = Backbone.View.extend({
 
   initialize: function(){
     // set up an event listener on the songs collection
-    this.collection.bind("change", this.handleQueueChange, this);
+    this.collection.on("change", this.handleQueueChange, this);
   },
 
   // templates are a nicer way to put js data into html strings
@@ -18,7 +18,7 @@ var PlayerView = Backbone.View.extend({
   // event listener
   handleQueueChange: function(){
     if(!this.model){
-      this.model = this.collection.queued()[0]
+      this.model = this.collection.queued()[0];
       this.render();
     }
   }
