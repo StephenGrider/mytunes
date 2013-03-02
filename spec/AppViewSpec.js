3a1b3eac2189d914bcd02c796a90586fc6638812
Whoaa512 <whoaa512@gmail.com>
2013-03-01 23:48:29 -0800

    Cleaned up unnecessary variables

--- spec/AppViewSpec.js
+++ spec/AppViewSpec.js
@@ -1,26 +1,26 @@
 describe('AppView', function() {
-  var view, app, fakeSongs, fakeSongData;
+  var appView, app;
 
   beforeEach(function () {
-    fakeSongData = [
-      {
-        artist: 'Fakey McFakerson',
-        title: 'Never Gonna Mock You Up',
-        url: 'example/url'
-      },
-      {
-        artist: 'BittyBacon',
-        title: 'Sizzle Sundays',
-        url: 'fake/url'
-      }
-    ];
-    fakeSongs = new Backbone.Collection(fakeSongData);
-    app = new App({library: fakeSongs});
-    view = new AppView({model: app});
+    app = new App({library:
+      new Backbone.Collection([
+        {
+          artist: 'Fakey McFakerson',
+          title: 'Never Gonna Mock You Up',
+          url: 'example/url'
+        },
+        {
+          artist: 'BittyBacon',
+          title: 'Sizzle Sundays',
+          url: 'fake/url'
+        }
+      ])
+    });
+    appView = new AppView({model: app});
   });
 
   it('should generate a PlayerView when created', function(){
-    expect(view.playerView).toEqual(jasmine.any(Object));
+    expect(appView.playerView).toEqual(jasmine.any(PlayerView));
   });
 
   xit('should change currentSong on \'play\' event' , function() {
