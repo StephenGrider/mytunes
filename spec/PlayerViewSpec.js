06efcdb3194e3a3c11ce188744e42aed0915673c
Whoaa512 <whoaa512@gmail.com>
2013-03-25 08:45:30 -0700

    Removed extra player spec

--- spec/PlayerViewSpec.js
+++ spec/PlayerViewSpec.js
@@ -19,6 +19,12 @@ describe('PlayerView', function() {
     appView = new AppView({model: new App({library: library})});
   });
 
+  it('gets its model property set to any song that is played', function(){
+    expect(appView.playerView.model).not.toEqual(library.at(0));
+    library.at(0).play();
+    expect(appView.playerView.model).toEqual(library.at(0));
+  });
+
   describe('Song transitions', function() {
     it('dequeues a song when finished playing & plays the next song', function(){
       library.at(0).play();
