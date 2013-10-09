acf9e583309aa44447c38fb859770488a84caa1b
phillipalexander <git@phillipalexander.io>
2013-10-09 08:21:35 -0700

    Host mp3 files on amazon s3

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 var songData = [
   {
-    url: "mp3s/08 4 Page Letter.mp3",
-    title: "4 Page Letter",
-    artist: "Aaliyah"
-  },
-  {
-    url: "mp3s/11 We Need A Resolution.mp3",
-    title: "We Need A Resolution",
-    artist: "Aaliyah"
-  },
-  {
-    url: "mp3s/05 Hot Like Fire.mp3",
-    title: "Hot Like Fire",
-    artist: "Aaliyah"
-  },
-  {
-    url: "mp3s/04 One In A Million.mp3",
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
     title: "One In A Million",
-    artist: "Aaliyah"
+    artist: "Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
+    title: "Age Ain't Nothing But A Number",
+    artist: "Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
+    title: "Hot Like Fire",
+    artist: "Aaliyah",
+  },
+  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
+    title: "If Your Girl Only Knew",
+    artist: "Aaliyah",
   }
 ];
